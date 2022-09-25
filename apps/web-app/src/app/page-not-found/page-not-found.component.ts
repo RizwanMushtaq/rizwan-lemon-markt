1f03b9190dd415bb95c8b62439a5de1adb9dfecd
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-09-25 21:41:14 +0200

    adding home and page-not-found component

---
+++ apps/web-app/src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rizwan-lemon-mart-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss'],
+})
+export class PageNotFoundComponent {}
