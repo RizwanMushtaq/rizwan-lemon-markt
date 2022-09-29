c1cc709565befd5825823759dc13278c07c457ef
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-09-29 18:28:57 +0200

    adding components in user module

---
+++ apps/web-app/src/app/user/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rizwan-lemon-mart-navigation-menu',
+  templateUrl: './navigation-menu.component.html',
+  styleUrls: ['./navigation-menu.component.scss'],
+})
+export class NavigationMenuComponent {}
