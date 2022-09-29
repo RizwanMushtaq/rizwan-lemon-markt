cb9d7295fada046dc6e721244aebe9d17adaa137
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-09-29 19:22:29 +0200

    adding pos component

---
+++ apps/web-app/src/app/pos/pos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rizwan-lemon-mart-pos',
+  templateUrl: './pos.component.html',
+  styleUrls: ['./pos.component.scss'],
+})
+export class PosComponent {}
--- apps/web-app/src/app/pos/pos.module.ts
+++ apps/web-app/src/app/pos/pos.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PosRoutingModule } from './pos-routing.module';
-
+import { PosComponent } from './pos.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    PosRoutingModule
-  ]
+  declarations: [PosComponent],
+  imports: [CommonModule, PosRoutingModule],
 })
-export class PosModule { }
+export class PosModule {}
